a768187b10e90cc4cff7ca75d23a3f523360ee8c
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-08-05 16:10:42 +0700

    [ADD] LoginView Cara Lain

--- src/features/login/LoginView.js
+++ src/features/login/LoginView.js
@@ -3,18 +3,26 @@ import { MainContext } from "../../context/MainContext";
 
 class LoginView extends Component {
   // contextTupe dfisini bertujuan utk baca context saat ini
-  static contextType = MainContext;
+  // static contextType = MainContext;
   // cara lain selain atas
 
   render() {
     // caranya ini
 
-    const { setProfile } = this.context;
+    // const { setProfile } = this.context;
     // sih setProfile akan jadi global
     return (
-      <div>
-        <button onClick={() => setProfile({ name: "doni" })}>Login</button>
-      </div>
+      <MainContext.Consumer>
+        {(ctx) => {
+          return (
+            <>
+              <button onClick={() => ctx.setProfile({ name: "doni" })}>
+                Login
+              </button>
+            </>
+          );
+        }}
+      </MainContext.Consumer>
     );
   }
 }
